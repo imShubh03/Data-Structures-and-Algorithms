4a7a3a5dac3bd565e27edff3875da46690ee7e12
Shubham7476 <shubhamsonake307@gmail.com>
2024-06-26 19:23:47 +0530

    binary search

---
+++ BInary Search/searchInRotatedSortedArray.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int searchInArray(vector<int> &arr, int target)
+    {
+        int n = arr.size();
+        int low = 0;
+        int high = n - 1;
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == target)
+            {
+                return mid;
+            }
+
+            if (arr[low] <= arr[mid])
+            {
+                if (arr[low] <= target && target <= arr[mid])
+                {
+                    high = low - 1;
+                }
+                else
+                {
+                    low = high + 1;
+                }
+            }
+            else
+            {
+                if (arr[mid] <= target && target <= arr[high])
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
+
+int main()
+{
+    int n;
+    cout << "enter total elements" << endl;
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "enter  elements in the array" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int target;
+    cout << "enter the target ?" << endl;
+
+    cin >> target;
+
+    Solution sol;
+    int result = sol.searchInArray(arr, target);
+
+    cout << "index will be " << result;
+
+    return 0;
+}
