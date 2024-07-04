1c360f1f540ea23040f7d34423dbee091fc92c3e
Shubham7476 <shubhamsonake307@gmail.com>
2024-07-04 23:04:53 +0530

    strings

--- Strings/reverseWordsInAString.cpp
+++ Strings/reverseWordsInAString.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+/*  time: O(N) , space: O(N)
 class Solution {
 public:
     string reverseWords(string str) {
@@ -28,6 +29,40 @@ public:
             ans += str.substr(i + 1, j - i);
         }
         return ans;
+    }
+};
+*/
+
+
+// time: O(N) and space: O(1)
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n = s.length();
+
+        // first reverse the entire
+        reverse(s.begin(), s.end());
+
+        int i = 0;
+        int l = 0;
+        int r = 0;
+
+        while(i<n){
+            while(i<n && s[i] != ' '){
+                s[r]=s[i];
+                i++;
+                r++;
+            }
+            if(l<r){
+                reverse(s.begin()+l,s.begin()+r);
+                s[r] = ' ';
+                r++;
+                l=r;
+            }
+            i++;
+        }
+        s=s.substr(0,r-1);
+        return s;
     }
 };
 
