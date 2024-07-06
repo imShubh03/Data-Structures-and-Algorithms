45e342e9ec1ddef99b5f2a12b5e8b870f058d914
Shubham7476 <shubhamsonake307@gmail.com>
2024-07-06 18:37:32 +0530

    string

--- Strings/replaceSpaces.cpp
+++ Strings/replaceSpaces.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+
+/*     TC: O(N)  SC:O(N)
 class Solution {
     public:
     string replaceSpaces(string &str){
@@ -19,7 +21,42 @@ class Solution {
         return temp;
     }
 };
+*/
 
+
+//TC:O(N)   SC:O(1)
+class Solution {
+public:
+    void replaceSpaces(string &str) {
+        int spaceCount = 0;
+        int originalLength = str.length();
+        
+        // Count spaces
+        for (int i = 0; i < originalLength; i++) {
+            if (str[i] == ' ') {
+                spaceCount++;
+            }
+        }
+
+        // Calculate new length of the string
+        int newLength = originalLength + spaceCount * 2;
+        str.resize(newLength); // Resize the string to the new length
+
+        // Replace spaces from the end
+        int j = newLength - 1;
+        for (int i = originalLength - 1; i >= 0; i--) {
+            if (str[i] == ' ') {
+                str[j] = '0';
+                str[j - 1] = '4';
+                str[j - 2] = '@';
+                j -= 3;
+            } else {
+                str[j] = str[i];
+                j--;
+            }
+        }
+    }
+};
 int main() {
 
     string str;
