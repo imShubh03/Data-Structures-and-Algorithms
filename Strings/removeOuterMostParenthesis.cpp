3e1d84d1b028a3446c61ef3291f04d4ac22ec0dd
Shubham7476 <shubhamsonake307@gmail.com>
2024-07-08 23:12:35 +0530

    strings

--- Strings/removeOuterMostParenthesis.cpp
+++ Strings/removeOuterMostParenthesis.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <string>
-#include<stack>
+#include <stack>
 
 using namespace std;
 
+/* time: O(N)  space: O(N)
 class Solution {
 public:
     string removeOuterParentheses(string s) {
@@ -25,8 +26,41 @@ public:
         return ans;
     }
 };
+*/
 
-int main() {
+// time O(N) , space:O(1) the string ans doesnt contributr to space
+class Solution
+{
+public:
+    string removeOuterParentheses(string s)
+    {
+        int cnt = 0;
+        string ans = "";
+        for (char ch : s)
+        {
+            if (ch == '(')
+            {
+                if (cnt > 0)
+                { // dont want the first '(' in ans
+                    ans += ch;
+                }
+                cnt++;
+            }
+            else if (ch == ')')
+            {
+                if (cnt > 1)
+                { // dont want the last ')' in ans
+                    ans += ch;
+                }
+                cnt--;
+            }
+        }
+        return ans;
+    }
+};
+
+int main()
+{
     Solution sol;
     string input = "(()())(())";
     string output = sol.removeOuterParentheses(input);
