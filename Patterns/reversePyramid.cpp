3a9c7515edce03300644c1e51e238a96f9162906
Shubham7476 <shubhamsonake307@gmail.com>
2024-07-16 17:12:18 +0530

    patterns

---
+++ Patterns/reversePyramid.cpp
@@ -0,0 +1,33 @@
+/*
+*********
+ *******
+  *****
+   ***
+    *
+*/
+#include <iostream>
+using namespace std;
+
+void nStarTriangle(int n) {
+    // Print an inverted pyramid pattern with stars
+    for(int i = 1; i <= n; i++) {
+        // Print leading spaces for each row
+        for(int j = 1; j < i; j++) {
+            cout << " ";
+        }
+        // Print stars for each row
+        for(int j = 1; j <= 2 * (n - i + 1) - 1; j++) {
+            cout << "*";
+        }
+        // No need for trailing spaces; they are not part of the pattern
+        cout << endl;
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter the size of the triangle: ";
+    cin >> n;
+    nStarTriangle(n);
+    return 0;
+}
