756445c788db9a104fd24c0f7d6325e1ed3df798
Shubham7476 <shubhamsonake307@gmail.com>
2024-07-18 01:37:15 +0530

    arrays

--- Arrays/KadanesAlgo_maxiSubarraySum.cpp
+++ Arrays/KadanesAlgo_maxiSubarraySum.cpp
@@ -46,7 +46,22 @@ class Solution{
 };
 */
 
+//optimal: KADANES ALGO  : time:O(n) space:O(1)
+class Solution{
+    public:
+    int maxSubArray(vector<int>&nums){
+        int sum =0;
+        int maxi = INT_MIN;
 
+        for(auto it:nums){
+            sum+=it;
+            maxi = max(maxi,sum);
+
+            if(sum<0) sum =0;
+        }
+        return maxi;
+    }
+};
 
 int main() {
     Solution sol;
