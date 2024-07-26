fda1065084f434625fa4583ea6ef50ec77b82834
Shubham7476 <shubhamsonake307@gmail.com>
2024-07-26 09:53:12 +0530

    array

--- Arrays/L1/lenOfLongeSubarrWithSumEquK.cpp
+++ Arrays/L1/lenOfLongeSubarrWithSumEquK.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-
+/*brute, time:O(n^2) space:O(1)
 class Solution {
 public:
     int lenOfLongSubarr(int arr[], int N, int K) {
@@ -19,6 +19,63 @@ public:
         return len;
     }
 };
+*/
+
+/*
+//optimized for (-ve , 0, +ve integers in array), time:O() space:O(1)
+class Solution {
+public:
+    int lenOfLongSubarr(int arr[], int N, int K) {
+        map<int,int>preSumMap;
+        int sum =0;
+        int maxlen=0;
+        for(int i =0;i<N;i++){
+            sum+=arr[i];
+            if(sum==K) {
+                maxlen=max(maxlen,i+1);
+            }
+            int remaining= sum-K;
+            if(preSumMap.find(remaining)!=preSumMap.end()){
+                int len = i - preSumMap[remaining];
+                maxlen=  max(maxlen,len);
+            }
+
+            if(preSumMap.find(sum)==preSumMap.end()){
+                preSumMap[sum]=i;
+            }
+        }
+        return maxlen;
+    }
+};
+*/
+
+
+//most optimal for (0 and +ve integers only)  time:O(2n)  space:O(1)
+class Solution {
+public:
+    int lenOfLongSubarr(int arr[], int N, int K){
+    int left = 0;
+    int right = 0;
+    int maxlen = 0;
+    int sum = 0;
+
+    while (right < N) {
+        sum += arr[right];
+
+        while (sum > K && left <= right) {
+            sum -= arr[left];
+            left++;
+        }
+
+        if (sum == K) {
+            maxlen = max(maxlen, right - left + 1);
+        }
+
+        right++;
+    }
+    return maxlen;
+    }
+};
 
 int main() {
     int n;
@@ -33,11 +90,11 @@ int main() {
 
     Solution sol;
     int K;
-    cout << "Enter the target sum: ";
+    cout << "Enter the target (K) : ";
     cin >> K;
 
     int len = sol.lenOfLongSubarr(arr, n, K);
-    cout << "Length of the longest subarray with sum equal to " << K << ": " << len << endl;
+    cout << "Length of the longest subarray with sum equal to " << K << " is " << len << endl;
 
     return 0;
 }
