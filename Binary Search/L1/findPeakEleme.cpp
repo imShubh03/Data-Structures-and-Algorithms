06310b33b8e4182c1b3bd05c0c46c2d2ad27d596
Shubham7476 <shubhamsonake307@gmail.com>
2024-07-30 11:55:40 +0530

    BS

--- Binary Search/L1/findPeakEleme.cpp
+++ Binary Search/L1/findPeakEleme.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/* brute : time:O(n)  space:O(1)
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
@@ -13,6 +14,37 @@ public:
         return -1;
     }
 };
+*/
+
+//optimised : time:O(logn) space:O(1)
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1) return 0;
+        if(nums[0]>nums[1]) return 0;
+        if(nums[n-2]<nums[n-1]) return n-1;
+
+        int low =1;
+        int high = n-2;
+        while(low<=high){
+            int mid = (low + high)/2;
+
+            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
+                return mid;
+            }
+            else if(nums[mid] >nums[mid-1]){
+                //we r on increasi curve so peak wi be on right
+                low = mid + 1;
+            }
+            else{
+                high = mid -1;
+            }
+        }
+        return -1;
+    }
+};
+
 
 int main() {
     Solution solution;
