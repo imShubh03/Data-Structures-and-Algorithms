9ad7f9611e0b98f81071d0e6f293a9fa2f957894
Shubham7476 <shubhamsonake307@gmail.com>
2024-09-02 11:40:01 +0530

    sw

--- Sliding Window/M1/longRepeatCharReplacement.cpp
+++ Sliding Window/M1/longRepeatCharReplacement.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/* brute: time:O(N^2)  space:O(26)
 class Solution {
 public:
     int characterReplacement(string s, int k) {
@@ -22,6 +23,65 @@ public:
                     break;
                 }
             }
+        }
+        return maxlen;
+    }
+};
+*/
+
+/*
+// optimised : time:O(2n) * 26 ,space:O(26)
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int n = s.length();
+        int l=0, r=0;
+        int maxlen=0, maxfreq=0;
+        int hash[26] ={0};
+        while(r < n){
+            hash[s[r] - 'A']++;
+            maxfreq= max(maxfreq, hash[s[r]- 'A']);
+            // check if valid 
+            // if not then trim down
+            while((r-l+1)- (maxfreq) > k){ //not valid
+                hash[s[l]- 'A']--;
+                maxfreq=0;
+                for(int i =0; i<26; i++){
+                    maxfreq = max(maxfreq, hash[i]);
+                }
+                l++;
+            }
+            if((r - l + 1) - (maxfreq) <= k){
+                maxlen = max(maxlen, r - l + 1);
+            }
+            r++;
+        }
+        return maxlen;
+    }
+};
+*/
+
+// most optimised time:O(n)
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int n = s.length();
+        int l=0, r=0;
+        int maxlen=0, maxfreq=0;
+        int hash[26] ={0};
+        while(r < n){
+            hash[s[r] - 'A']++;
+            maxfreq= max(maxfreq, hash[s[r]- 'A']);
+            // check if valid 
+            // if not then trim down
+            if((r-l+1)- (maxfreq) > k){ //not valid
+                hash[s[l]- 'A']--;
+                l++;
+            }
+            if((r - l + 1) - (maxfreq) <= k){
+                maxlen = max(maxlen, r - l + 1);
+            }
+            r++;
         }
         return maxlen;
     }
