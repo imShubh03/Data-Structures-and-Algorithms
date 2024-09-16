53d1fb01fc28442cd6062fa3961de4ee2f1eca21
Shubham7476 <shubhamsonake307@gmail.com>
2024-09-16 10:38:06 +0530

    array

--- Arrays/L1/remove-Duplicates-From-SortedArr.cpp
+++ Arrays/L1/remove-Duplicates-From-SortedArr.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/* brute: time:O(nlogn)+O(n)   space:O(n)
 int removeDuplicates(int arr[], int n){
     set<int>st;
     for(int i =0; i<n; i++){
@@ -11,8 +12,20 @@ int removeDuplicates(int arr[], int n){
         arr[ind]= it;
         ind++;
     }
-
     return ind;
+}
+*/
+// optimal: 2 pointers time:O(n) space:O(1)
+int removeDuplicates(int arr[], int n){
+    int i =0;
+    // first ele already in correct pos so j from 1
+    for(int j =1; j<n; j++){
+        if(arr[i] != arr[j]){
+            arr[i+1]= arr[j];
+            i++;
+        }
+    }
+    return i+1;
 }
 
 int main() {
