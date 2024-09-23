6ee5ce300fb09d4ba09037ee92019169240c1d38
Shubham7476 <shubhamsonake307@gmail.com>
2024-09-23 17:41:45 +0530

    array

--- Arrays/L3/3-Sum.cpp
+++ Arrays/L3/3-Sum.cpp
@@ -55,6 +55,44 @@ public:
 };
 */
 
+//optimal time ~ O(n^2) + O(n logn)  space: O(1) {ans to return so ignored}
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>>ans;
+        sort(nums.begin(), nums.end());
+
+        for(int i =0; i<n; i++){
+            // if is are same then move
+            if(i>0 && nums[i] == nums[i-1]) continue;
+
+            int j = i + 1;
+            int k = n - 1;
+
+            while(j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum < 0){
+                    j++;
+                }
+                else if(sum > 0){
+                    k--;
+                }
+                else{
+                    vector<int>temp = {nums[i], nums[j], nums[k]};
+                    ans.push_back(temp);
+                    j++;
+                    k--;
+                    // if j and k are equal to prev then
+                    while(j<k && nums[j] == nums[j-1]) j++;
+                    while(j < k && nums[k] == nums[k+1]) k--;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
 int main() {
     int n;
     cin >> n;
